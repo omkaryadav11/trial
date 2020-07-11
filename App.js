16f075560fe0067bbc4530874fc47d327829e124
Omkar110 <oy970831@gmail.com>
2020-07-11 14:28:15 +0530

    first coomit

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import login from './components/login';
+import signup from './components/signup';
+import home from './components/home';
+import fire from './config/fire';
+export default class App extends React.Component {
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  constructor(props)
+  {
+    super(props)
+    this.state={
+      user : { }
+    }
+  }
+   
+  componentDidMount() 
+  {
+    this.authListener();
+  }
+  authListener() {
+    fire.auth().onAuthStateChanged((user)=> {
+      if(user)
+      {
+        this.setState({user})
+      }
+      else 
+      {
+        this.setState({user : null})
+      }
+    })
+  }
+  render() {
+    const stack = createStackNavigator();
+ 
+    return (
+      <NavigationContainer>
+      <stack.Navigator screenOptions={{ headerShown: false }}>
+
+      
+        <stack.Screen name="home" component={home} />
+        <stack.Screen name="login" component={login} />
+        <stack.Screen name="signup" component={signup} />
+      
+        
+      
+      </stack.Navigator>
+    </NavigationContainer>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/signup.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from 'react'
+import { StatusBar } from 'expo-status-bar';
+
+import { StyleSheet, Text, View,Image, Button,TextInput,TouchableOpacity } from 'react-native';
+
+export default class login extends React.Component {
+  render(){
+    return (
+    <View style={styles.container}>
+     
+      <View style={styles.view1}>
+        <Image style={{width:250,height:250}} source={require("../assets/logo.png")} />
+
+        <View style={{flexDirection:"row"}}>
+          <TouchableOpacity style={{position:"absolute",bottom:-60,right:70}} onPress={()=>{this.props.navigation.navigate("login")}}>
+            <Text style={{color:"yellow",fontWeight:"700",fontSize:20,} } >LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{position:"absolute",bottom:-60,left:65}}>
+            <Text style={{color:"yellow",fontWeight:"700",fontSize:20}} >SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+
+      <View style={styles.view2}>
+        <TextInput placeholder="Full name" style={[styles.username,{marginBottom:30}]} />
+        <TextInput placeholder="Password" style={[styles.username,{marginBottom:30}]} />
+        <TextInput placeholder="Password" style={[styles.username,{marginBottom:30}]} />
+        <Button title="Login in" color="black"/>
+      </View>
+
+
+      <StatusBar style="light" />
+    </View>
+  );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+  },
+  view1:{
+    flex:3,
+    backgroundColor:"#000000",
+    alignItems:"center",
+    justifyContent:"center"
+    
+  },
+  view2:{
+    flex:2,
+    backgroundColor:"#ffffff",
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  username:{
+    width:"80%",
+    borderWidth:1,
+    height:45,
+    borderRadius:23,
+    padding:5,
+    textAlign:"center"
+  }
+});
